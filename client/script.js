3017a10f23ea2623c7cb42e2b8a463293a30cfef
Santiago Spinelli <spinelli.spi98@gmail.com>
2026-07-18 13:03:01 -0300

    Connect frontend to Render

--- client/script.js
+++ client/script.js
@@ -27,7 +27,7 @@ contactForm.addEventListener("submit", async (event) => {
     const name = document.querySelector("#name").value;
     const email = document.querySelector("#email").value;
     const message = document.querySelector("#message").value;
-    const response = await fetch("http://localhost:3000/contact", {
+    const response = await fetch("https://portfolio-cm1m.onrender.com/contact", {
 
         method: "POST",
 
